583e24ee10d1aeaa1f6b3bdd6ffe44905e45dc25
DollyPy <ddolapo1@gmail.com>
2023-05-03 05:55:50 +0100

    training data

--- model_data.py
+++ model_data.py
@@ -23,14 +23,14 @@ def add_bg_from_local(image_file):
     unsafe_allow_html=True
     )
 def make_pred(image_path):
-    class_codes={
-                0:"Apple_Healthy", 1:"Apple_Scab", 2:"Apple_Black_rot", 3:"Apple_Cedar_rust", 
-                4:"Corn_Healthy", 5:"Corn_Cercospora_leaf", 6:"Corn_Common_rust", 7:"Corn_Northern_Leaf_Blight",
-                8:"Grape_Healthy", 9:"Grape_Black_Rut", 10:"Esca",11:"Grape_Blight",
-                12:"Pepper_Healthy", 13:"Pepper_Bacterial_Spot",
-                14:"Potato_Healthy", 15:"Potato_P_Early_Blight",16:"Potato_P_Late_Blight",
-                17:"Tomato_Healthy", 18:"Tomato_Early_Blight", 19:"Tomato_Leaf_Mold", 20:"Tomato_Target_Spot"
-            }
+        class_codes={
+        0:"Apple_Healthy", 1:"Apple_Scab", 2:"Apple_Black_rot", 3:"Apple_Cedar_rust", 
+        4:"Corn_Healthy", 5:"Corn_Cercospora_leaf", 6:"Corn_Common_rust", 7:"Corn_Northern_Leaf_Blight",
+        8:"Grape_Healthy", 20:"Grape_Black_Rut", 9:"Esca",10:"Grape_Blight",
+        1`:"Pepper_Healthy", 12:"Pepper_Bacterial_Spot",
+        13:"Potato_Healthy", 14:"Potato_Early_Blight",15:"Potato_Late_Blight",
+        16:"Tomato_Healthy", 17:"Tomato_Early_Blight", 18:"Tomato_Leaf_Mold",19:"Tomato_Mosaic_virus"
+    }
     image = np.asarray([img_to_array(image_path)/255,])
     obj_class = np.argmax(model.predict(image[0:1]))
     return class_codes[obj_class]
